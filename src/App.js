b71136685e1d7357799d2b123dc9f47e0c17fdb0
Lilia <lzvalerievna@gmail.com>
2021-07-08 22:14:45 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,96 +1,99 @@
 
 import React,{useEffect,useState}  from 'react';
 import 'antd/dist/antd.css';
-import { Pagination} from 'antd';
+import { Pagination, Tabs} from 'antd';
 import { debounce } from 'lodash';
 import InputSearch from './components/search/search';
-import TabPanel from './components/tabpanel/tabpanel';
 import MoviesList from './components/moviesList/movieslist';
 import './App.css';
 import ErrorFetch from './components/errorFetch/errorFetch';
 import Spinner from './components/spin/spin';
 
+const { TabPane } = Tabs;
+
+
 
 
 export default function App(){
-const [movies, setMovies] = useState([])
-const [loading, setLoading] = useState(true)
-const [termSearch,setTermSearch] = useState('return')
-const [err, setErr] = useState(false)
-const [errorMessage, setErrorMessage] = useState('')
-const [errorMessage2, setErrorMessage2] = useState('')
-const [currentPage, setCurrentPage] = useState(1)
-const [totalPage, setTotalPage] = useState(0)
-
-
-const moviePage = `https://api.themoviedb.org/3/search/movie?api_key=7ffce4f49b66e12f59dd06c5256de3c2&language=en-US&query=${termSearch}`
-
- const getDefaultMovies = async (url) => {
-
-  try{
-      const res = await fetch(url);
-      if(!res.ok) {
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [termSearch,setTermSearch] = useState('return')
+  const [err, setErr] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const [errorMessage2, setErrorMessage2] = useState('')
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalPage, setTotalPage] = useState(0)
+  
+  
+  const moviePage = `https://api.themoviedb.org/3/search/movie?api_key=7ffce4f49b66e12f59dd06c5256de3c2&language=en-US&query=${termSearch}`
+  
+   const getDefaultMovies = async (url) => {
+  
+    try{
+        const res = await fetch(url);
+        if(!res.ok) {
+          setErr(true)
+          setErrorMessage2('The search did not  any results')
+          console.log(res.status)
+        }
+        const body =  await res.json()
+        return body;
+    }catch (error) {
         setErr(true)
-        setErrorMessage2('The search did not  any results')
-        console.log(res.status)
-      }
-      const body =  await res.json()
-      return body;
-  }catch (error) {
-      setErr(true)
-      setErrorMessage('Could not fetch, check the connection')
-      console.log("Could not fetch")
-      return false;
+        setErrorMessage('Could not fetch, check the connection')
+        console.log("Could not fetch")
+        return false;
+    }
+    
   }
   
-}
-
-useEffect(() =>{
-  setLoading(true)
-  getDefaultMovies(moviePage)
- .then((data) => {
-   setMovies(data.results)
-   setLoading(false)
-   setErr(false)
- });
-}, []);
-
-
-const handleOnInput = (ev, pageNumber) => { 
-    const text = ev.target.value
-    const getMovies = `https://api.themoviedb.org/3/search/movie?api_key=7ffce4f49b66e12f59dd06c5256de3c2&language=en-US&query=${text}&page=${pageNumber}`
-      setLoading(true)
-      setErr(false)
-      if(text !== '') {
-        getDefaultMovies(getMovies)    
-        .then(data => {
-        if(data.results.length === 0) {
-        setMovies([])
-        setErr(true)
-        setErrorMessage2('The search did not  any results')
-        }else {   
+  useEffect(() =>{
+    setLoading(true)
+    getDefaultMovies(moviePage)
+   .then((data) => {
+     setMovies(data.results)
+     setLoading(false)
+     setErr(false)
+   });
+  }, []);
+  
+  
+  const handleOnInput = (ev, pageNumber) => { 
+      const text = ev.target.value
+      const getMovies = `https://api.themoviedb.org/3/search/movie?api_key=7ffce4f49b66e12f59dd06c5256de3c2&language=en-US&query=${text}&page=${pageNumber}`
+        setLoading(true)
+        setErr(false)
+        if(text !== '') {
           getDefaultMovies(getMovies)    
-          .then(response => {
-          setLoading(false)
-          setTermSearch(text)
-          setMovies(response.results)
-          setTotalPage(response.total_results)
-          console.log(response.total_results)
-         })
-        }
-     })
+          .then(data => {
+          if(data.results.length === 0) {
+          setMovies([])
+          setErr(true)
+          setErrorMessage2('The search did not  any results')
+          }else {   
+            getDefaultMovies(getMovies)    
+            .then(response => {
+            setLoading(false)
+            setTermSearch(text)
+            setMovies(response.results)
+            setTotalPage(response.total_results)
+            console.log(response.total_results)
+           })
+          }
+       })
+      }
     }
-  }
-
-  const nextPage = async(pageNumber,value) => {
-         const getMovies2 =  `https://api.themoviedb.org/3/search/movie?api_key=7ffce4f49b66e12f59dd06c5256de3c2&language=en-US&query=${value}&page=${pageNumber}`
-         getDefaultMovies(getMovies2)
-         .then(data => {
-          setMovies(data.result)
-          setCurrentPage(pageNumber)
-         })
-
-  };
+  
+    const nextPage = async(pageNumber,value) => {
+           const getMovies2 =  `https://api.themoviedb.org/3/search/movie?api_key=7ffce4f49b66e12f59dd06c5256de3c2&language=en-US&query=${value}&page=${pageNumber}`
+           getDefaultMovies(getMovies2)
+           .then(data => {
+            setMovies(data.results)
+            setCurrentPage(pageNumber)
+           })
+  
+    };
+  
 
   const handleOnChange = debounce(handleOnInput, 1000)
   
@@ -99,17 +102,23 @@ const handleOnInput = (ev, pageNumber) => {
 
     return (
       <div>
-        <TabPanel/>
-        < InputSearch handleOnChange={handleOnChange}/>
-        {err ? <ErrorFetch errorMessage = {errorMessage} errorMessage2 = {errorMessage2}/> : null}
-        {loading ? <Spinner /> : null}
-        {hasData ?  < MoviesList movies={movies} /> : null}
-        {totalPage > 20 ?   < Pagination 
-        defaultCurrent={1}
-        current = {currentPage}
-        onChange = {(page) => nextPage(page, termSearch)}
-        total={totalPage}
-        /> : ''}
+        <Tabs defaultActiveKey="1" centered>
+           <TabPane  tab="Search" key="1">
+                    < InputSearch handleOnChange={handleOnChange}/>
+                    {err ? <ErrorFetch errorMessage = {errorMessage} errorMessage2 = {errorMessage2}/> : null}
+                    {loading ? <Spinner /> : null}
+                    {hasData ?  < MoviesList movies={movies} /> : null}
+                    {totalPage > 20 ?   < Pagination 
+                     defaultCurrent={1}
+                     current = {currentPage}
+                     onChange = {(page) => nextPage(page, termSearch)}
+                     total={totalPage}
+                     /> : ''}
+           </TabPane>
+           <TabPane tab="Rated" key="2">
+                  Content of Tab Pane 2
+           </TabPane>
+        </Tabs>
       </div>
     )
 }
